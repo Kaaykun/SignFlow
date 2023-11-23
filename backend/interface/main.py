bbc957bc347e4361bf49d84235651ea87ba3a2b3
Jaris Fenner <jaris.a.fenner@gmail.com>
2023-11-23 17:05:04 +0900

    Initial package setup from Jupyter testing

---
+++ backend/interface/main.py
@@ -0,0 +1,41 @@
+import os
+# Suppress WARNING, INFO, and DEBUG messages related to tensorflow
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+from sklearn.model_selection import train_test_split
+
+from backend.params import *
+from backend.ml_logic.data import load_wlas_df, load_features_df, load_selected_df
+from backend.ml_logic.preprocessor import create_X
+from backend.ml_logic.encoders import categorize_y
+from backend.ml_logic.augment import augment_data
+from backend.ml_logic.registry import test_shape_X_y, test_shape_X_y_val, test_shape_X_y_aug
+
+
+def main():
+    wlas_df = load_wlas_df()
+
+    features_df = load_features_df(wlas_df)
+
+    selected_df, input_length = load_selected_df(features_df)
+
+    X = create_X(selected_df, input_length)
+    y_cat = categorize_y(selected_df, input_length)
+
+    test_shape_X_y(selected_df, input_length, X, y_cat)
+
+    X_train, X_val, y_cat_train, y_cat_val = train_test_split(X,
+                                                            y_cat,
+                                                            train_size=TRAIN_SIZE,
+                                                            random_state=1,
+                                                            stratify=y_cat)
+
+    test_shape_X_y_val(X, X_val, y_cat, y_cat_val)
+
+    X_aug, y_aug = augment_data(X_train, y_cat_train)
+
+    test_shape_X_y_aug(X_aug, X_train, y_aug, y_cat_train)
+
+
+if __name__ == '__main__':
+    main()
---
+++ backend/ml_logic/registry.py
@@ -0,0 +1,97 @@
+import cv2
+import shutil
+import sys
+
+from backend.params import *
+
+
+def generate_processed_videos(X):
+    """
+    Generate processed videos from sampled frames.
+
+    Parameters:
+    - X (numpy.ndarray): Array containing sampled frames for multiple videos.
+    - output_folder (str): Path to the folder to store processed videos. Defaults to '../data/processed_videos/'.
+
+    Returns:
+    - None
+    """
+    def frames_to_video(sampled_frames, output_path, fps=FRAMES_PER_VIDEO):
+        height, width, _ = sampled_frames[0].shape
+        fourcc = cv2.VideoWriter.fourcc(*'mp4v')
+        video = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+
+        for frame in sampled_frames:
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            video.write(frame_rgb)
+
+        video.release()
+
+    output_folder = os.path.dirname('../data/processed_videos/')
+
+    if os.path.exists(output_folder):
+        shutil.rmtree(output_folder)
+
+    os.makedirs(output_folder)
+
+    for i, sampled_frames in enumerate(X):
+            video_path = f'../data/processed_videos/processed_{i}.mp4'
+            frames_to_video(sampled_frames, video_path)
+
+
+def generate_csv(list_of_dataframes):
+    """
+    Generate CSV files from a list of DataFrames.
+
+    Parameters:
+    - list_of_dataframes (list): List containing pandas DataFrames.
+
+    Returns:
+    - None
+    """
+    output_folder = os.path.dirname('../data/csv/')
+
+    if os.path.exists(output_folder):
+        shutil.rmtree(output_folder)
+
+    os.makedirs(output_folder)
+
+    for i, dataframe in enumerate(list_of_dataframes):
+        file_path = f'{output_folder}/dataframe_{i}.csv'
+        dataframe.to_csv(file_path, index=False)
+
+
+def test_shape_X_y(selected_df, input_length, X, y_cat):
+    if X.shape == (len(selected_df), FRAMES_PER_VIDEO, *TARGET_SIZE, 3):
+        print(f'✅ X has been initialized with Shape {X.shape}!')
+    else:
+        sys.exit('❌ X has not been initialized properly!')
+
+    if y_cat.shape == (input_length, N_CLASSES):
+        print(f'✅ y has been initialized with Shape {y_cat.shape}!')
+    else:
+        sys.exit('❌ y has not been initialized properly!')
+
+
+def test_shape_X_y_val(X, X_val, y_cat, y_cat_val):
+    if X_val.shape == (round(len(X) * (1 - TRAIN_SIZE)), FRAMES_PER_VIDEO, *TARGET_SIZE, 3):
+        print(f'\n✅ X_val has been initialized with Shape {X_val.shape}!')
+    else:
+        sys.exit('\n❌ X_val has not been initialized properly!')
+
+    if y_cat_val.shape == (round(len(y_cat) * (1 - TRAIN_SIZE)), N_CLASSES):
+        print(f'✅ y_cat_val has been initialized with Shape {y_cat_val.shape}!')
+    else:
+        sys.exit('❌ y_cat_val has not been initialized properly!')
+
+
+def test_shape_X_y_aug(X_aug, X_train, y_aug, y_cat_train):
+    if X_aug.shape == ((NUMBER_OF_AUGMENTATIONS + 1) * len(X_train), FRAMES_PER_VIDEO, *TARGET_SIZE, 3):
+        print(f'\n✅ X_aug has been initialized with Shape {X_aug.shape}!')
+    else:
+        sys.exit('\n❌ X_aug has not been initialized properly!')
+
+    if y_aug.shape == ((NUMBER_OF_AUGMENTATIONS + 1) * len(y_cat_train), N_CLASSES):
+        print(f'✅ y_aug has been initialized with Shape {y_aug.shape}!')
+    else:
+        sys.exit('❌ y_aug has not been initialized properly!')

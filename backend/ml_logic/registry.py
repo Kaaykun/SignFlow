8fd9fa32c8eebec173e7018ef4250409be4aca64
Jaris Fenner <jaris.a.fenner@gmail.com>
2023-11-24 14:44:28 +0900

    Added save and load model functions

--- backend/ml_logic/registry.py
+++ backend/ml_logic/registry.py
@@ -2,8 +2,13 @@ import cv2
 import shutil
 import sys
 import os
+import time
+import glob
+from tensorflow import keras
+from google.cloud import storage
 
 from backend.params import FRAMES_PER_VIDEO, TARGET_SIZE, TRAIN_SIZE, N_CLASSES, NUMBER_OF_AUGMENTATIONS
+from backend.params import LOCAL_REGISTRY_PATH
 
 
 def generate_processed_videos(X):
@@ -96,3 +101,64 @@ def test_shape_X_y_aug(X_aug, X_train, y_aug, y_cat_train):
         print(f'✅ y_aug has been initialized with Shape {y_aug.shape}!')
     else:
         sys.exit('❌ y_aug has not been initialized properly!')
+
+
+def save_model(model):
+    """
+    Persist trained model locally on the hard drive at f"{LOCAL_REGISTRY_PATH}{timestamp}.h5"
+    - if MODEL_TARGET='gcs', also persist it in your bucket on GCS at "models/{timestamp}.h5"
+    - if MODEL_TARGET='mlflow', also persist it on MLflow instead of GCS
+    """
+
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+    # Save model locally
+    model_path = os.path.join(LOCAL_REGISTRY_PATH, f"{timestamp}.h5")
+    model.save(model_path)
+    print(model_path, 'updated function 2')
+    print(model_path.split("/")[-1])
+    print("✅ Model saved locally")
+
+    # if MODEL_TARGET == "gcs":
+    #     print('successfully entered function')
+    #     model_filename = model_path.split("/")[-1]
+    #     print(f'model filename = {model_filename}')
+    #     client = storage.Client()
+    #     print(f'client = {client}')
+    #     bucket = client.bucket(BUCKET_NAME)
+    #     print(f'bucket = {bucket}')
+    #     blob = bucket.blob(f'models/{model_filename}')
+    #     print(f'blob = {blob}')
+    #     blob.upload_from_filename(model_path)
+
+    #     print("✅ Model saved to GCS")
+
+    #     return None
+
+    return None
+
+
+def load_model():
+    """
+    Return a saved model:
+    - locally (latest one in alphabetical order)
+    - or from GCS (most recent one) if MODEL_TARGET=='gcs'  --> for unit 02 only
+    - or from MLFLOW (by "stage") if MODEL_TARGET=='mlflow' --> for unit 03 only
+
+    Return None (but do not Raise) if no model is found
+
+    """
+    # Get the latest model version name by the timestamp on disk
+    local_model_directory = os.path.join(LOCAL_REGISTRY_PATH, "models")
+    local_model_paths = glob.glob(f"{local_model_directory}/*")
+
+    if not local_model_paths:
+        return None
+
+    most_recent_model_path_on_disk = sorted(local_model_paths)[-1]
+
+    latest_model = keras.models.load_model(most_recent_model_path_on_disk)
+
+    print("✅ Model loaded from local disk")
+
+    return latest_model
--- backend/params.py
+++ backend/params.py
@@ -1,6 +1,5 @@
 import os
 
-
 ##################  VARIABLES  ##################
 # Class definition
 SELECTED_WORDS = ['work','study', 'write', 'hot', 'cold', 'family']
@@ -13,7 +12,19 @@ NUMBER_OF_AUGMENTATIONS = 3
 # Train split parameters
 TRAIN_SIZE = 0.7
 
+###############  GCP VARIABLES  #################
+# DATA_SIZE = os.environ.get("DATA_SIZE")
+MODEL_TARGET = os.environ.get("MODEL_TARGET")
+GCP_PROJECT = os.environ.get("GCP_PROJECT")
+GCP_REGION = os.environ.get("GCP_REGION")
+# BQ_DATASET = os.environ.get("BQ_DATASET")
+# BQ_REGION = os.environ.get("BQ_REGION")
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+# INSTANCE = os.environ.get("INSTANCE")
 
-##################  CONSTANTS  #####################
+
+#################  CONSTANTS  ####################
 MAIN_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data', '')
 VIDEO_PATH = os.path.join(MAIN_PATH, 'videos', '')
+# LOCAL_REGISTRY_PATH = os.path.join(MAIN_PATH, 'models', '')
+LOCAL_REGISTRY_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'models')
